d49492dbf49b447c058a79b73a84f4522ccca7e4
Kish Siva <rodes88@msn.com>
2019-04-17 22:55:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,9 +69,14 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do ||
-    
-  end 
+  
+  holiday_hash.each do |season, holiday|
+    puts season 
+    puts holiday.keys
+    holiday.each do |hols, equip|
+      puts equip
+    end 
+  end  
 
 end
 
@@ -79,6 +84,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  
+  
 end
 
 
