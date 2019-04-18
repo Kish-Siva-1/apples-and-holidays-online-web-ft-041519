7d7c75fc4e32dbab497c2ca0ca3d5be027113cac
Kish Siva <rodes88@msn.com>
2019-04-18 00:30:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,8 +84,8 @@ def all_supplies_in_holidays(holiday_hash)
   
   
   holiday_hash.each do |season, holiday|
-    puts season.keys
-    puts holiday
+    puts season
+    puts holiday.keys
     holiday.each do |hols, equip|
       puts equip
     end 
